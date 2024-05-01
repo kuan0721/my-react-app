0bcb51e89f23cb4f9ec830f2ad7879c3428d3964
kuan0721 <thomaskuan0721@gmail.com>
2024-05-01 14:50:34 +0800

    qqqq

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import MySlider from "./MySlider";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>Javascript程式設計，使用react</h1>
+      <h2>第一個元件</h2>
+      <span>R: </span><MySlider />
+      <span>G: </span><MySlider />
+      <span>B: </span><MySlider />
+      {/* <header className="App-header"> */}
+      {/* <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello React! 2024.5.1</h1>
         <a
           className="App-link"
@@ -14,8 +20,9 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
+
+      {/* </header> */}
     </div>
   );
 }
