9961c042cd58eb9519dcf9c4e33a8687b36f8deb
kuan0721 <thomaskuan0721@gmail.com>
2024-05-07 18:52:25 +0800

    q

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import MySlider from "./MySlider";
 function App() {
   return (
     <div className="App">
-      <h1>Javascript程式設計，使用react</h1>
+      <h1>5b0g0019管浩任</h1>
       <h2>第一個元件</h2>
       <span>R: </span><MySlider />
       <span>G: </span><MySlider />
       <span>B: </span><MySlider />
+
+      
+
+
+
       {/* <header className="App-header"> */}
       {/* <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello React! 2024.5.1</h1>
