7afe309dd897e81a2f419d7c7b033c8e95a55235
kuan0721 <thomaskuan0721@gmail.com>
2024-05-28 22:22:38 +0800

    cccc

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <h1>5b0g0019</h1>
         <h1>管浩任</h1>
         <MyRGBPanel />
-        <h2>MyCalculator</h2>
+        <h2>計算機</h2>
         <MyCalculator />
       </header>
     </div>
