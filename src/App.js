eed62af7b862660325695c4c5f3b8daf2bcb4ab7
kuan0721 <thomaskuan0721@gmail.com>
2024-06-03 17:09:45 +0800

    20240603

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import './App.css';
-import MyRGBPanel from './components/MyRGBPanel';
-import MyCalculator from './components/MyCalculator';
+import "./App.css";
+import MyRGBPanel from "./components/rgb-panel/MyRGBPanel";
+import MySlider from "./components/rgb-panel/MySlider";
+import MyCalculator from "./components/calculator/MyCalculator";
+import TicTacToe from "./components/tic-tac-toe/TicTacToe";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello React! 2024.5.28</h1>
-        <h1>5b0g0019</h1>
-        <h1>管浩任</h1>
-        <MyRGBPanel />
-        <h2>計算機</h2>
-        <MyCalculator />
-      </header>
+      <h1>MySlider</h1>
+      <MySlider />
+      <h1>MyRGBPanel</h1>
+      <MyRGBPanel />
+      <h1>MyCalculator</h1>
+      <MyCalculator />
+      <h1>Tic-Tac-Toe Game</h1>
+      <TicTacToe />
     </div>
   );
 }
